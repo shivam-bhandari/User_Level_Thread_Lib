d7bd4102b555734fa94678f5fc029c2e9c940e72
Shivam Bhandari <shivambhandari@gmail.com>
2023-02-02 12:12:07 -0800

    All functions except dequeue and iterate

--- p2/libuthread/queue.c
+++ p2/libuthread/queue.c
@@ -15,7 +15,6 @@ struct Node {
 	struct Node* next;
 }
 
-
 // The LL to store the queue
 // Using rear pointer to insert at the end 
 struct queue {
@@ -26,14 +25,15 @@ struct queue {
 };
 
 bool queueEmpty(queue_t queue){
-	return queue_length(queue) == 0;
+	return queue->len == 0;
 }
 
 queue_t queue_create(void)
 {
 	/* TODO Phase 1 */
-	struct queue* q = (struct queue*)malloc(sizeof(struct queue));
-    q->front = q->rear = NULL;
+	queue_t q = (queue_t)malloc(sizeof(struct queue));
+    q->front = NULL;
+	q->rear = NULL;
 	q->len = 0;
     return q;
 }
@@ -41,12 +41,12 @@ queue_t queue_create(void)
 int queue_destroy(queue_t queue)
 {
 	/* TODO Phase 1 */
-	if (queueEmpty){
+	if (queueEmpty(queue) || !queue){
 		return -1;
 	}
 	while(!queueEmpty(queue)) {
-		free(queueFront(queue));
-		queuePop(queue);
+		free(FRONTOFQUEUE(queue)); // IMPLEMENT FUNCTION
+		queue_dequeue(queue);
 	}
 	return 0;
 }
@@ -57,47 +57,108 @@ int queue_enqueue(queue_t queue, void *data)
 	if ( !data || !queue) 
 		return -1;
 
-	// create new node, and assign data
+	// create new node
 	Node* newNode = malloc(sizeof(Node));
+
+	// check for any malloc error
+	if(!newNode) 
+	{
+		printf("malloc error\n");
+		return -1;
+	}
+	
+	// assign value 
 	newNode->data = data;
 
-	if(!newNode)
-		return -1
 
 	if (queueEmpty(queue)) {
 		queue->rear = NULL;
 		queue->front = newNode;
-		newNode->next = newNode->prev = NULL;
+		newNode->next = NULL;
 	}
-
 	else {
 		queue->rear->next = newNode;
-		newNode->prev = queue->rear;
-		queue->rear = newNode;
-		newNode->next = NULL;
+		queue->rear = queue->rear->next;
+		queue->rear->next = NULL;
 	} 
 
-	queue->size = queue->size + 1;
+	queue->len++;
 	return 0;
 }
 
+// @TRISHNA IMPLEMENT THIS
+
 int queue_dequeue(queue_t queue, void **data)
 {
+	if(!queue || **data == NULL) 
+	{
+		printf("queue or data NULL; return -1\n")
+		return -1;
+	}
+	if(front exists) {
+		
+	}
 	/* TODO Phase 1 */
 }
 
 int queue_delete(queue_t queue, void *data)
 {
 	/* TODO Phase 1 */
+	int flag = 0; // flag to check if data is found
+
+	if(!data || !queue)
+		return -1;
+
+	Node* temp = queue->front;
+
+	// if queue has only one element
+
+	if(temp->next == NULL && && temp->data == data){
+		flag = 1;
+		queue->back = NULL;
+		queue->front = NULL;
+		free(temp);
+		return 0;
+	}
+
+	while(temp!=NULL){
+
+		if (temp-> data == data)
+		{
+			flag = 1;
+			node *T = temp;
+			temp->prev->next = T->next;
+
+			free(T);
+			break;
+		}
+		temp = temp->next;
+	}
+		
+	queue->size--;
+	if(flag == 0)
+		return flag;
+	return -1;
+	
 }
 
 int queue_iterate(queue_t queue, queue_func_t func)
 {
 	/* TODO Phase 1 */
+	// if(!queue || !func)
+	// 	return -1;
+	
+	// // iterate through queue
+
+
+
 }
 
 int queue_length(queue_t queue)
 {
+	if(!queue)
+		return -1;
 	/* TODO Phase 1 */
+	return queue->len;
 }
 
