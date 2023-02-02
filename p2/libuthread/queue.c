4031660ee6fb210743015466f154253e1d707e5e
Shivam Bhandari <shivambhandari@gmail.com>
2023-02-01 22:44:19 -0800

    Initial Changes to queue.c

--- p2/libuthread/queue.c
+++ p2/libuthread/queue.c
@@ -4,23 +4,81 @@
 
 #include "queue.h"
 
+// We implement the queue using a LL for all operations to be in O(1) 
+// and to navigate forwards and backwards in the queue.
+
+// queue_t is a pointer to an object of struct queue type
+
+// Linked List Node
+struct Node {
+	void* data;
+	struct Node* next;
+}
+
+
+// The LL to store the queue
+// Using rear pointer to insert at the end 
 struct queue {
 	/* TODO Phase 1 */
+	struct Node* front;
+	struct Node* rear;
+	int len;
 };
+
+bool queueEmpty(queue_t queue){
+	return queue_length(queue) == 0;
+}
 
 queue_t queue_create(void)
 {
 	/* TODO Phase 1 */
+	struct queue* q = (struct queue*)malloc(sizeof(struct queue));
+    q->front = q->rear = NULL;
+	q->len = 0;
+    return q;
 }
 
 int queue_destroy(queue_t queue)
 {
 	/* TODO Phase 1 */
+	if (queueEmpty){
+		return -1;
+	}
+	while(!queueEmpty(queue)) {
+		free(queueFront(queue));
+		queuePop(queue);
+	}
+	return 0;
 }
 
 int queue_enqueue(queue_t queue, void *data)
 {
 	/* TODO Phase 1 */
+	if ( !data || !queue) 
+		return -1;
+
+	// create new node, and assign data
+	Node* newNode = malloc(sizeof(Node));
+	newNode->data = data;
+
+	if(!newNode)
+		return -1
+
+	if (queueEmpty(queue)) {
+		queue->rear = NULL;
+		queue->front = newNode;
+		newNode->next = newNode->prev = NULL;
+	}
+
+	else {
+		queue->rear->next = newNode;
+		newNode->prev = queue->rear;
+		queue->rear = newNode;
+		newNode->next = NULL;
+	} 
+
+	queue->size = queue->size + 1;
+	return 0;
 }
 
 int queue_dequeue(queue_t queue, void **data)
